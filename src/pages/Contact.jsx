23dee4dd6bed49c2909dd0de785097c37661a85a
Nona Pareulidze <nona.pareulidze1@gmil.com>
2026-06-08 17:34:42 +0400

    feat: add map iframe to Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -26,7 +26,7 @@ export default function Contact() {
                 Address : <a href="#">1892 Chenoweth Drive TN</a>
               </p>
               <p>
-                Phone : <a href="cal:123-456-3451">123-456-3451</a>
+                Phone : <a href="tel:123-456-3451">123-456-3451</a>
               </p>
             </div>
             <button className={styles.mapbutton}>View on Map</button>
@@ -41,14 +41,16 @@ export default function Contact() {
                 Address : <a href="#">1892 Chenoweth Drive TN</a>
               </p>
               <p>
-                Phone : <a href="cal:123-456-3451">123-456-3451</a>
+                Phone : <a href="tel:123-456-3451">123-456-3451</a>
               </p>
             </div>
             <button className={styles.mapbutton}>View on Map</button>
           </div>
         </div>
       </div>
-      
+      <div className={styles.map}>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3106.924995064334!2d-78.14309835476865!3d38.85707387643792!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b5cfbe8fdd760d%3A0x3856e12aa6ff0e82!2sWines%20Ln%2C%20Chester%20Gap%2C%20VA%2022623%2C%20USA!5e0!3m2!1sen!2sge!4v1780924739833!5m2!1sen!2sge" width="600" height="450" style={{border:0}} allowFullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
       <Footer />
     </main>
   );
