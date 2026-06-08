cdb1cda8f0701ddd2797e9df91194121936cc538
Nona Pareulidze <nona.pareulidze1@gmil.com>
2026-06-08 22:15:44 +0400

    feat: add burger menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,63 @@
 import Label from "../Label/Label";
-import {Link} from 'react-router-dom'
-import logo from '../../assets/logo.svg'
-import style from './Header.module.css' 
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.svg";
+import style from "./Header.module.css";
+import { useState ,useEffect} from "react";
+import { useLocation } from "react-router-dom";
+export default function Header({ pageName }) {
+  const [burger, setBurger] = useState(false);
+  const location = useLocation();
+  const pathname = location.pathname;
+  useEffect(() => {
+      if (burger) {
+        document.body.classList.add(style.no_scroll);
+      } else {
+        document.body.classList.remove(style.no_scroll);
+      }
+    }, [burger])
 
-export default function Header({pageName}) {
-    return(<>
-    <Label page={pageName}></Label>
-    <header className={style.header}>
-        <Link to='/'><img src={logo}></img></Link>
-        <nav className={style.nav}>
-        <Link to='/portfolio'>Portfolio</Link>
-        <Link to='/about'>About</Link>
-        <Link to='/contact'>Contact</Link>
+  return (
+    <>
+      <Label page={pageName}></Label>
+      <header className={style.header}>
+        <Link to="/">
+          <img src={logo}></img>
+        </Link>
+        <nav className={`${style.nav} ${burger ? style.navactive : ""}`}>
+          <Link
+            to="/portfolio"
+            className={pathname === "/portfolio" ? style.active : ""}
+            onClick={() => setBurger(false)}
+          >
+            Portfolio
+          </Link>
+          <Link
+            to="/about"
+            className={pathname === "/about" ? style.active : ""}
+            onClick={() => setBurger(false)}
+          >
+            About
+          </Link>
+          <Link
+            to="/contact"
+            className={pathname === "/contact" ? style.active : ""}
+            onClick={() => setBurger(false)}
+          >
+            Contact
+          </Link>
+          
         </nav>
-   
-    </header>
-   
-    </>)
+        <div
+          onClick={() => setBurger(!burger)}
+          className={`${style.burger} ${burger ? style.burgeractive : ""}`}
+        >
+          <span className={style.line}></span>
+          <span className={style.line}></span>
+          <span className={style.line}></span>
+        </div>
+      </header>
+      <div className={style.blackovelay} onClick={() => setBurger(false)}></div>
+      
+    </>
+  );
 }
